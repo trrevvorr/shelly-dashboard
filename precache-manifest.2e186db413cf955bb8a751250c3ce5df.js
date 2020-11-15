18370730897f5e8fabd525a2b1852028ffbc21a8
Trevor Ross <theboss@amazon.com>
2020-11-15 16:02:08 -0500

    Initial dist subtree commit

---
+++ precache-manifest.2e186db413cf955bb8a751250c3ce5df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30c733cc69e9894ecc43",
+    "url": "/shelly-dashboard/css/app.59135f41.css"
+  },
+  {
+    "revision": "759251a7210547121d5be1538d3a5fed",
+    "url": "/shelly-dashboard/index.html"
+  },
+  {
+    "revision": "30c733cc69e9894ecc43",
+    "url": "/shelly-dashboard/js/app.a2a3f176.js"
+  },
+  {
+    "revision": "69f31821af6bcee36788",
+    "url": "/shelly-dashboard/js/chunk-vendors.f6c91a48.js"
+  },
+  {
+    "revision": "9e441ca314e31ab29fc78ddb4f66abae",
+    "url": "/shelly-dashboard/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/shelly-dashboard/robots.txt"
+  }
+]);
