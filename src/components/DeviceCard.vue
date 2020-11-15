392a7fede4ba30c2e93ec504437e1065ed190022
Trevor Ross <theboss@amazon.com>
2020-11-15 10:03:14 -0500

    added initial assets

---
+++ src/components/DeviceCard.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="card">
+    <h3>Some Device</h3>
+    <a :href="`http://${ip}/`">details</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    ip: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.card {
+  background-color: #2c3e50;
+  padding: 1rem;
+  border-radius: 1rem;
+}
+
+h3  {
+  margin: 0.25rem 0;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h1>Shelly Dashboard</h1>
+    <div class="devices">
+      <DeviceCard ip="192.168.86.227" />
+      <DeviceCard ip="192.168.86.77" />
+      <DeviceCard ip="192.168.86.999" />
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import DeviceCard from '@/components/DeviceCard.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    DeviceCard
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  text-align: center;
+}
+
+.devices {
+  display: grid;
+  grid-gap: 1rem;
+  max-width: 1200px;
+  margin: 0 1rem;
+}
+
+@media (min-width: 600px) {
+  .devices { grid-template-columns: repeat(2, 1fr); }
+}
+
+@media (min-width: 900px) {
+  .devices { grid-template-columns: repeat(3, 1fr); }
+}
+
+</style>
